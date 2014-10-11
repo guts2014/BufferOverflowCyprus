e2b25587679bf70c3b7ddeca2bbf0228c757bb48
Skeftical <apoelistas1@hotmail.com>
2014-10-11 22:33:40 +0100

    added-error
    About wifi,3g,bluetooth

--- iBeaconSocializer/gen/com/spartanapps/ibeaconsocializer/R.java
+++ iBeaconSocializer/gen/com/spartanapps/ibeaconsocializer/R.java
@@ -64,6 +64,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int alert=0x7f050004;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int title_activity_update=0x7f050003;
